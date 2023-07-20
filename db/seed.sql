531f7972af8205d4a9559b2bd5755bb7106a385a
terenceDev <terence.arseneault@hotmail.com>
2023-07-20 14:43:20 -0300

    added data for database, seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,27 @@
 USE employeeTracker_db;
 
-INSERT INTO department
+INSERT INTO department(name)
+VALUE   ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal")
+
+INSERT INTO role(title, salary, department_id)
+VALUE   ("Sales Lead"),
+        ("Salesperson"),
+        ("Lead Engineer"),
+        ("Software Engineer"),
+        ("Account Manager"),
+        ("Accountant"),
+        ("Legal Team Lead"),
+        ("Lawer")
+
+INSERT INTO  employee(first_name, last_name, role_id, manager_id)
+VALUE   ("John", "Doe", 1, null),
+        ("Mike", "Chan", 2, 1 ),
+        ("Ashley", "Rodriguez", 3, null),
+        ("Kevin", "Tupik", 4, 3),
+        ("Kunal", "Singh", 5, null),
+        ("Malia", "Brown", 6, 5),
+        ("Sarah", "Lourd", 7, null),
+        ("Tom", "Allen", 8, 7)
